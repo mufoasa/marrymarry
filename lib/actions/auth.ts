c5c8921f35c5f8c4aac88d4f299b4e8ed57bfc09
mufoasa <96444527+mufoasa@users.noreply.github.com>
2026-02-07 23:33:43 +0100

    Update auth.ts

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -17,7 +17,7 @@ export async function signUp(formData: FormData) {
     password,
     options: {
       emailRedirectTo: process.env.NEXT_PUBLIC_DEV_SUPABASE_REDIRECT_URL || 
-        `${process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'}/auth/callback`,
+        `${process.env.NEXT_PUBLIC_SITE_URL || 'https://marry.mk/'}/auth/callback`,
       data: {
         full_name: fullName,
         role: role,
