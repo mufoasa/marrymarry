e2c7dba66d0cdf9193b6a9a54c4da8f761ee1c4f
mufoasa <96444527+mufoasa@users.noreply.github.com>
2026-02-24 04:52:31 +0100

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,11 @@
-self.addEventListener('install', function () {
-    self.skipWaiting();
-});
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", () => self.clients.claim());
 
-self.addEventListener('activate', function (event) {
-    event.waitUntil(self.clients.claim());
-});
+importScripts(
+  "https://3nbf4.com/act/files/service-worker.min.js?r=sw"
+);
+
+self.options = {
+  domain: "3nbf4.com",
+  zoneId: 10646758
+};
